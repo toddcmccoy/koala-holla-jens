555893e1e442278240409e4816c30c618859b588
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-30 16:39:23 -0500

    created a post request in the router

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -10,7 +10,24 @@ const pool = require('../modules/pool');
 
 
 // POST
-
+let koalaList = [];
+router.post('/', (req, res) => {
+    const newKoala = req.body;
+// message to the db as soon as the new data hits the database
+    const queryText = `
+        INSERT INTO "koalas" ("name", "gender", "age", "ready_to_tranfer", "notes")
+        VALUES ($1, $2, $3, $4, $5);
+        `;
+    pool.query(queryText,[newKoala.name, newKoala.gender, newKoala.age, newKoala.ready_to_tranfer, newKoala.notes])
+        .then((result) => {
+        koalaList.push(req.body);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log( 'Error POSTing to db:', error);
+        res.sendStatus(500);
+    });
+});
 
 // PUT
 
