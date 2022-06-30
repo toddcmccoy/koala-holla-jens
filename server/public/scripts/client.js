1996d99e84268485bdbf6cc7d530dd6539ef8967
Maria Isabel Gonzalez <isabel_133@hotmail.com>
2022-06-30 16:46:14 -0500

    created post request in client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -36,5 +36,28 @@ function getKoalas(){
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
- 
+  // Get info to send to the server
+    const newKoala = {
+        name: $('#nameIn').val(), 
+        gender: $('#genderIn').val(),
+        age: $('#ageIn').val(),
+        ready_to_tranfer: $('#readyToTransgerIn').val(),
+        notes: $('#notesIn').val()
+    };
+
+    console.log('Adding koala in client', newKoala);
+
+    // Send the new koala to the server as data
+    $.ajax({
+        method: 'POST',
+        url: '/koala',
+        data: newKoala,
+    }).then(function(response) {
+        console.log(response);
+        getKoalas();
+    }).catch(function(error) {
+        console.log('error in koala post', error); 
+        alert('Error adding koala in client. Please try again later.')       
+    });
 }
+ 
